8df18f7ef6be185a309362645fb1c9bf3737e8ff
Nicholas Edwards <nicholas6598@gmail.com>
2020-07-10 22:36:12 -0400

    content component build

---
+++ src/POPOSList.js
@@ -0,0 +1,42 @@
+import React from "react";
+import POPOSSpace from "./POPOSSpace";
+
+
+function POPOSList(){
+    return(
+        <div fileName="POPOSList">
+            <POPOSSpace
+                name = "50 California Street"
+                address = "50 California St."
+                image = "50-california-st.jpg"
+            />
+            <POPOSSpace
+                name = "100 Pine"
+                address = "100 Pine St."
+                image = "100-pine.jpg"
+            />
+            <POPOSSpace
+                name = "101 California Street"
+                address = "101 California St."
+                image = "101-california.jpg"
+            />
+            <POPOSSpace
+                name = "343 Sansome Roof Garden"
+                address = "343 Sansome St."
+                image = "343-sansome-roof-garden.jpg"
+            />
+            <POPOSSpace
+                name = "525 Market Street Plaza"
+                address = "525 Market St."
+                image = "525-market-street-plaza.jpg"
+            />
+            <POPOSSpace
+                name = "Citigroup Center"
+                address = "1 Sansome St."
+                image = "citigroup-center.jpg"
+            />
+        </div>
+    )
+}
+
+export default POPOSList;
